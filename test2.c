0e647e926c586851ba6c8eb1e8559223676b9a66
Makotox0207 <xiaoxishicheng@gmail.com>
2023-01-29 22:09:09 +0900

    演算

---
+++ test2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// 演算のプログラム
+int main() {
+  printf("%d + %d = %d\n", 5, 2, 5 + 2);
+  printf("%d - %d = %d\n", 5, 2, 5 - 2);
+  printf("%d / %d = %d 余り %d\n", 5, 2, 5 / 2, 5 % 2);
+}
+
