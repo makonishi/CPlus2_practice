f3da18f397e9e46fb2287bf53cfde185dc1d49d2
Makotox0207 <xiaoxishicheng@gmail.com>
2023-01-29 23:00:18 +0900

    if~else
    fix

---
+++ test7.c
@@ -0,0 +1,15 @@
+// if~else
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("数値を入力: ");
+    // 整数を標準入力
+    scanf("%d",&a);
+    // 入力した値が正の数かどうか調べる 
+    if(a > 0){
+        printf("入力した値は正の数です。\n");  // 正の数なら実行
+    } else {
+      printf("入力した値は正の数ではありません。\n");
+    }
+}
